9c0c32b7f7f86f3a563aa5358aac58a14deb6487
emafaillace97 <emafaillace2016@outlook.com>
2024-12-01 18:48:24 -0300

    Se agregaron pruebas de la clase Rectangulo

--- src/ejercicios_ppt1/Main.java
+++ src/ejercicios_ppt1/Main.java
@@ -15,6 +15,12 @@ public class Main {
             System.out.println(EjerciciosPPT1.formaProgresionAritmetica(arreglo));
             System.out.println(EjerciciosPPT1.getSumaPares(arreglo));
             System.out.println(EjerciciosPPT1.getVolumenEsfera(4));
+            Rectangulo r1 = new Rectangulo(5,4, 0, 0);
+            System.out.println(r1.calcularArea());
+            System.out.println(r1.contienePunto(2,2));
+            r1.mover(6,6);
+            System.out.println(r1.calcularArea());
+            System.out.println(r1.contienePunto(90,90));
         }catch(ArregloVacioEx | NumerosNegativosEx | RadioIncorrectoEx e){
             System.out.println(e.getMessage());
         }
